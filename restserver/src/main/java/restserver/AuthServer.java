fbdd36297720e4d1c866afb8ddd1dba44d03a831
Adam_Szczeblewski <Adam_Szczeblewski@epam.com>
2017-04-02 19:00:23 +0200

    rudimentary registration process in OAuth server

--- restserver/src/main/java/restserver/AuthServer.java
+++ restserver/src/main/java/restserver/AuthServer.java
@@ -1,5 +1,6 @@
 package restserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -10,26 +11,33 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.security.Principal;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Collections.singleton;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-@EnableResourceServer
 @RestController
 public class AuthServer {
-
-//    extends WebMvcConfigurerAdapter
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/oauth/confirm_access").setViewName("authorize");
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServer.class);
@@ -40,17 +48,34 @@ public class AuthServer {
         return principal;
     }
 
-//    @Controller
-//    protected static class Registration {
-//        @GetMapping("/register")
-//        String register(String username, @RequestParam String redirect) {
-//            return "redirect:" + redirect;
-//        }
-//    }
+    @Configuration
+    protected static class MvcConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/registration").setViewName("registrationForm");
+        }
+    }
+
+    @Controller
+    protected static class Registration {
+
+        @Autowired
+        UserService userService;
+
+        @PostMapping("/register")
+        String register(@RequestParam String username, @RequestParam String password) {
+            userService.create(username, password);
+            final String redirect_uri = "http://localhost:9001/ui";//should be a param
+            return "redirect:" + redirect_uri;
+        }
+    }
 
     @Configuration
     @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
     protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        UserService userService;
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -60,6 +85,7 @@ public class AuthServer {
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userService);
             auth.inMemoryAuthentication()
                     .withUser("joe").password("joe").roles("SUPERVISOR");
         }
@@ -72,6 +98,7 @@ public class AuthServer {
         }
     }
 
+    @EnableResourceServer
     @Configuration
     @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
     protected static class ResourceServerConfig extends ResourceServerConfigurerAdapter {
@@ -79,8 +106,28 @@ public class AuthServer {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
+                    .antMatchers("/registrationForm.html").permitAll()
+                    .antMatchers("/register").permitAll()
                     .antMatchers("/login").permitAll()
                     .anyRequest().authenticated();
+        }
+    }
+
+    @Component
+    protected static class UserService implements UserDetailsService {
+
+        Map<String, UserDetails> users = new ConcurrentHashMap<>();
+
+        @Override
+        public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+            if (!users.containsKey(s)) throw new UsernameNotFoundException(s);
+            return users.get(s);
+        }
+
+        void create(String username, String password) {
+            users.putIfAbsent(username, new User(
+                    username, password,
+                    singleton(new SimpleGrantedAuthority("ROLE_USER"))));
         }
     }
 }
--- restserver/src/main/java/restserver/Tester.java
+++ restserver/src/main/java/restserver/Tester.java
@@ -1,12 +1,12 @@
 package restserver;
 
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.client.RestClientResponseException;
 
 import static restserver.RestCall.restGet;
+import static restserver.RestCall.restPostVoid;
 
 public class Tester {
 
@@ -14,7 +14,15 @@ public class Tester {
 
     public static void main(String[] args) {
         try {
-            withUserPass();
+            System.out.println("Login as existing user");
+            oauthLogin("joe", "joe");
+            //register and login with new user
+            final String user = "fluffy";
+            final String password = "joe";
+            System.out.println("Register new user");
+            register(user, password);
+            System.out.println("Login as new user");
+            oauthLogin(user, password);
         } catch (RestClientResponseException e) {
             System.err.println(e.getRawStatusCode());
             System.err.println(e.getStatusText());
@@ -22,26 +30,30 @@ public class Tester {
         }
     }
 
-    private static void withUserPass() {
+    private static void register(String user, String password) {
+        restPostVoid(ROOT + "/register")
+                .param("username", user)
+                .param("password", password)
+                .voidExecute();
+    }
+
+    private static void oauthLogin(String user, String password) {
 
         ResourceOwnerPasswordResourceDetails res = new ResourceOwnerPasswordResourceDetails();
         res.setClientId("SOMEAPP");
         res.setClientSecret("SECRET");
         res.setAccessTokenUri(ROOT + "/oauth/token");
-        res.setUsername("joe");
-        res.setPassword("joe");
+        res.setUsername(user);
+        res.setPassword(password);
 
-        dance(ROOT, res);
-    }
-
-    private static void dance(String root, OAuth2ProtectedResourceDetails res) {
         OAuth2RestTemplate t = new OAuth2RestTemplate(res);
         final OAuth2AccessToken accessToken = t.getAccessToken();
-        System.out.println(accessToken);
+        System.out.println("Access token: " + accessToken);
 
-        String p = restGet(root + "/whois", String.class)
+        String p = restGet(ROOT + "/whois", String.class)
                 .restTemplate(() -> t)
                 .execute();
-        System.out.println(p);
+        System.out.println("User: " + p);
     }
+
 }
